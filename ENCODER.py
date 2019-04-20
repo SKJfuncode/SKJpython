33021ac28545361b9b28c70e546f0e953ef7455b
Satyam kumar jha <49813806+SKJfuncode@users.noreply.github.com>
2019-04-20 20:42:25 +0530

    Add files via upload

---
+++ ENCODER.py
@@ -0,0 +1,380 @@
+import random
+alphabet=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','q','x','y','z']
+wn=random.randint(27,74)
+ab=random.choice(alphabet)
+print(ab)
+print(wn)
+if ab=='a':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9,wn+10,wn+11,wn+12,wn+13,wn+14,wn+15,wn+16,wn+17,wn+18,wn+19,wn+20,wn+21,wn+22,wn+23,wn+24,wn+25
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='b':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9,wn+10,wn+11,wn+12,wn+13,wn+14,wn+15,wn+16,wn+17,wn+18,wn+19,wn+20,wn+21,wn+22,wn+23,wn+24
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='c':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9,wn+10,wn+11,wn+12,wn+13,wn+14,wn+15,wn+16,wn+17,wn+18,wn+19,wn+20,wn+21,wn+22,wn+23
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='d':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9,wn+10,wn+11,wn+12,wn+13,wn+14,wn+15,wn+16,wn+17,wn+18,wn+19,wn+20,wn+21,wn+22
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='e':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9,wn+10,wn+11,wn+12,wn+13,wn+14,wn+15,wn+16,wn+17,wn+18,wn+19,wn+20,wn+21
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='f':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9,wn+10,wn+11,wn+12,wn+13,wn+14,wn+15,wn+16,wn+17,wn+18,wn+19,wn+20
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='g':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9,wn+10,wn+11,wn+12,wn+13,wn+14,wn+15,wn+16,wn+17,wn+18,wn+19
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='h':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9,wn+10,wn+11,wn+12,wn+13,wn+14,wn+15,wn+16,wn+17,wn+18
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='i':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9,wn+10,wn+11,wn+12,wn+13,wn+14,wn+15,wn+16,wn+17
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='j':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9,wn+10,wn+11,wn+12,wn+13,wn+14,wn+15,wn+16
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='k':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn+10,wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9,wn+10,wn+11,wn+12,wn+13,wn+14,wn+15
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='l':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-11,wn+10,wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9,wn+10,wn+11,wn+12,wn+13,wn+14
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='m':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-12,wn-11,wn+10,wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9,wn+10,wn+11,wn+12,wn+13
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='n':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-13,wn-12,wn-11,wn+10,wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9,wn+10,wn+11,wn+12
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='o':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-14,wn-13,wn-12,wn-11,wn+10,wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9,wn+10,wn+11
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='p':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-15,wn-14,wn-13,wn-12,wn-11,wn+10,wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9,wn+10
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='q':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-16,wn-15,wn-14,wn-13,wn-12,wn-11,wn+10,wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8,wn+9
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='r':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-17,wn-16,wn-15,wn-14,wn-13,wn-12,wn-11,wn+10,wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7,wn+8
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='s':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-18,wn-17,wn-16,wn-15,wn-14,wn-13,wn-12,wn-11,wn+10,wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6,wn+7
+elif ab=='t':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-19,wn-18,wn-17,wn-16,wn-15,wn-14,wn-13,wn-12,wn-11,wn+10,wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5,wn+6
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='u':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-20,wn-19,wn-18,wn-17,wn-16,wn-15,wn-14,wn-13,wn-12,wn-11,wn+10,wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4,wn+5
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='v':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-21,wn-20,wn-19,wn-18,wn-17,wn-16,wn-15,wn-14,wn-13,wn-12,wn-11,wn+10,wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3,wn+4
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='w':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-22,wn-21,wn-20,wn-19,wn-18,wn-17,wn-16,wn-15,wn-14,wn-13,wn-12,wn-11,wn+10,wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2,wn+3
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='x':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-23,wn-22,wn-21,wn-20,wn-19,wn-18,wn-17,wn-16,wn-15,wn-14,wn-13,wn-12,wn-11,wn+10,wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1,wn+2
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='y':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-24,wn-23,wn-22,wn-21,wn-20,wn-19,wn-18,wn-17,wn-16,wn-15,wn-14,wn-13,wn-12,wn-11,wn+10,wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0,wn+1
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+elif ab=='z':
+	a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z=wn-25,wn-24,wn-23,wn-22,wn-21,wn-20,wn-19,wn-18,wn-17,wn-16,wn-15,wn-14,wn-13,wn-12,wn-11,wn+10,wn-9,wn-8,wn-7,wn-6,wn-5,wn-4,wn-3,wn-2,wn-1,wn+0
+	print(a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z)
+
+pt=str(input("--> :"))
+et1=(pt.replace("","."))
+et2=(et1.replace(" ",","))
+
+# Replacing the aplhabets witn numbers
+et3=(et2.replace("a",str(a)))
+et4=(et3.replace("b",str(b)))
+et5=(et4.replace("c",str(c)))
+et6=(et5.replace("d",str(d)))
+et7=(et6.replace("e",str(e)))
+et8=(et7.replace("f",str(f)))
+et9=(et8.replace("g",str(g)))
+et10=(et9.replace("h",str(h)))
+et11=(et10.replace("i",str(i)))
+et12=(et11.replace("j",str(j)))
+et13=(et12.replace("k",str(k)))
+et14=(et13.replace("l",str(l)))
+et15=(et14.replace("m",str(m)))
+et16=(et15.replace("n",str(n)))
+et17=(et16.replace("o",str(o)))
+et18=(et17.replace("p",str(p)))
+et19=(et18.replace("q",str(q)))
+et20=(et19.replace("r",str(r)))
+et21=(et20.replace("s",str(s)))
+et22=(et21.replace("t",str(t)))
+et23=(et22.replace("u",str(u)))
+et24=(et23.replace("v",str(v)))
+et25=(et24.replace("w",str(w)))
+et26=(et25.replace("x",str(x)))
+et27=(et26.replace("y",str(y)))
+et28=(et27.replace("z",str(z)))
+bet=et28
+
+# Replacing the numbers with binary
+bet0=(bet.replace("1",str(bin(1))))
+bet1=(bet0.replace("2",str(bin(2))))
+bet2=(bet1.replace("3",str(bin(3))))
+bet3=(bet2.replace("4",str(bin(4))))
+bet4=(bet3.replace("5",str(bin(5))))
+bet5=(bet4.replace("6",str(bin(6))))
+bet6=(bet5.replace("7",str(bin(7))))
+bet7=(bet6.replace("8",str(bin(8))))
+bet8=(bet7.replace("9",str(bin(9))))
+bet9=(bet8.replace("10",str(bin(10))))
+bet10=(bet9.replace("11",str(bin(11))))
+bet11=(bet10.replace("12",str(bin(12))))
+bet12=(bet11.replace("13",str(bin(13))))
+bet13=(bet12.replace("14",str(bin(14))))
+bet14=(bet13.replace("15",str(bin(15))))
+bet15=(bet14.replace("16",str(bin(16))))
+bet16=(bet15.replace("17",str(bin(17))))
+bet17=(bet16.replace("18",str(bin(18))))
+bet18=(bet17.replace("19",str(bin(19))))
+bet19=(bet18.replace("20",str(bin(20))))
+bet20=(bet19.replace("21",str(bin(21))))
+bet21=(bet20.replace("22",str(bin(22))))
+bet22=(bet21.replace("23",str(bin(23))))
+bet23=(bet22.replace("24",str(bin(24))))
+bet24=(bet23.replace("25",str(bin(25))))
+bet25=(bet24.replace("26",str(bin(26))))
+bet26=(bet25.replace("27",str(bin(27))))
+bet27=(bet26.replace("28",str(bin(28))))
+bet28=(bet27.replace("29",str(bin(29))))
+bet29=(bet28.replace("30",str(bin(30))))
+bet30=(bet29.replace("31",str(bin(31))))
+bet31=(bet30.replace("32",str(bin(32))))
+bet32=(bet31.replace("33",str(bin(33))))
+bet33=(bet32.replace("34",str(bin(34))))
+bet34=(bet33.replace("35",str(bin(35))))
+bet35=(bet34.replace("36",str(bin(36))))
+bet36=(bet35.replace("37",str(bin(37))))
+bet37=(bet36.replace("38",str(bin(38))))
+bet38=(bet37.replace("39",str(bin(39))))
+bet39=(bet38.replace("40",str(bin(40))))
+bet40=(bet39.replace("41",str(bin(41))))
+bet41=(bet40.replace("42",str(bin(42))))
+bet42=(bet41.replace("43",str(bin(43))))
+bet43=(bet42.replace("44",str(bin(44))))
+bet44=(bet43.replace("45",str(bin(45))))
+bet45=(bet44.replace("46",str(bin(46))))
+bet46=(bet45.replace("47",str(bin(47))))
+bet47=(bet46.replace("48",str(bin(48))))
+bet48=(bet47.replace("49",str(bin(49))))
+bet49=(bet48.replace("50",str(bin(50))))
+bet50=(bet49.replace("51",str(bin(51))))
+bet51=(bet50.replace("52",str(bin(52))))
+bet52=(bet51.replace("53",str(bin(53))))
+bet53=(bet52.replace("54",str(bin(54))))
+bet54=(bet53.replace("55",str(bin(55))))
+bet55=(bet54.replace("56",str(bin(56))))
+bet56=(bet55.replace("57",str(bin(57))))
+bet57=(bet56.replace("58",str(bin(58))))
+bet58=(bet57.replace("59",str(bin(59))))
+bet59=(bet58.replace("60",str(bin(60))))
+bet60=(bet59.replace("61",str(bin(61))))
+bet61=(bet60.replace("62",str(bin(62))))
+bet62=(bet61.replace("63",str(bin(63))))
+bet63=(bet62.replace("64",str(bin(64))))
+bet64=(bet63.replace("65",str(bin(65))))
+bet65=(bet64.replace("66",str(bin(66))))
+bet66=(bet65.replace("67",str(bin(67))))
+bet67=(bet66.replace("68",str(bin(68))))
+bet68=(bet67.replace("69",str(bin(69))))
+bet69=(bet68.replace("70",str(bin(70))))
+bet70=(bet69.replace("71",str(bin(71))))
+bet71=(bet70.replace("72",str(bin(72))))
+bet72=(bet71.replace("73",str(bin(73))))
+bet73=(bet72.replace("74",str(bin(74))))
+bet74=(bet73.replace("75",str(bin(75))))
+bet75=(bet74.replace("76",str(bin(76))))
+bet76=(bet75.replace("77",str(bin(77))))
+bet77=(bet76.replace("78",str(bin(78))))
+bet78=(bet77.replace("79",str(bin(79))))
+bet79=(bet78.replace("80",str(bin(80))))
+bet80=(bet79.replace("81",str(bin(81))))
+bet81=(bet80.replace("82",str(bin(82))))
+bet82=(bet81.replace("83",str(bin(83))))
+bet83=(bet82.replace("84",str(bin(84))))
+bet84=(bet83.replace("85",str(bin(85))))
+bet85=(bet84.replace("86",str(bin(86))))
+bet86=(bet85.replace("87",str(bin(87))))
+bet87=(bet86.replace("88",str(bin(88))))
+bet88=(bet87.replace("89",str(bin(89))))
+bet89=(bet88.replace("90",str(bin(90))))
+bet90=(bet89.replace("91",str(bin(91))))
+bet91=(bet90.replace("92",str(bin(92))))
+bet92=(bet91.replace("93",str(bin(93))))
+bet93=(bet92.replace("94",str(bin(94))))
+bet94=(bet93.replace("95",str(bin(95))))
+bet95=(bet94.replace("96",str(bin(96))))
+bet96=(bet95.replace("97",str(bin(97))))
+bet97=(bet96.replace("98",str(bin(98))))
+bet98=(bet97.replace("99",str(bin(99))))
+bet99=(bet98.replace("100",str(bin(100))))
+bet100=(bet99.replace("101",str(bin(101))))
+bet101=(bet100.replace("0b",""))
+print(bet101)
+bet101=hext
+# Replacing binary with hexadecimal
+def rip_0b(num):
+	rip0=bin(num)
+	rip=int(rip0b.replace("0b",""))
+	return rip
+hext0=(hext.replace(rip_0b(1),str(hex(rip_0b(1))))
+hext=(hext0.replace(rip_0b(2),str(hex(rip_0b(2))))
+hext2=(hext.replace(rip_0b(3),str(hex(rip_0b(3))))
+hext3=(hext2.replace(rip_0b(4),str(hex(rip_0b(4))))
+hext4=(hext3.replace(rip_0b(5),str(hex(rip_0b(5))))
+hext5=(hext4.replace(rip_0b(6),str(hex(rip_0b(6))))
+hext6=(hext5.replace(rip_0b(7),str(hex(rip_0b(7))))
+hext7=(hext6.replace(rip_0b(8),str(hex(rip_0b(8))))
+hext8=(hext7.replace(rip_0b(9),str(hex(rip_0b(9))))
+hext9=(hext8.replace(rip_0b(10),str(hex(rip_0b(10))))
+hext10=(hext9.replace(rip_0b(11),str(hex(rip_0b(11))))
+hext11=(hext10.replace(rip_0b(12),str(hex(rip_0b(12))))
+hext12=(hext11.replace(rip_0b(13),str(hex(rip_0b(13))))
+hext13=(hext12.replace(rip_0b(14),str(hex(rip_0b(14))))
+hext14=(hext13.replace(rip_0b(15),str(hex(rip_0b(15))))
+hext15=(hext14.replace(rip_0b(16),str(hex(rip_0b(16))))
+hext16=(hext15.replace(rip_0b(17),str(hex(rip_0b(17))))
+hext17=(hext16.replace(rip_0b(18),str(hex(rip_0b(18))))
+hext18=(hext17.replace(rip_0b(19),str(hex(rip_0b(19))))
+hext19=(hext18.replace(rip_0b(20),str(hex(rip_0b(20))))
+hext20=(hext19.replace(rip_0b(21),str(hex(rip_0b(21))))
+hext21=(hext20.replace(rip_0b(22),str(hex(rip_0b(22))))
+hext22=(hext21.replace(rip_0b(23),str(hex(rip_0b(23))))
+hext23=(hext22.replace(rip_0b(24),str(hex(rip_0b(24))))
+hext24=(hext23.replace(rip_0b(25),str(hex(rip_0b(25))))
+hext25=(hext24.replace(rip_0b(26),str(hex(rip_0b(26))))
+hext26=(hext25.replace(rip_0b(27),str(hex(rip_0b(27))))
+hext27=(hext26.replace(rip_0b(28),str(hex(rip_0b(28))))
+hext28=(hext27.replace(rip_0b(29),str(hex(rip_0b(29))))
+hext29=(hext28.replace(rip_0b(30),str(hex(rip_0b(30))))
+hext30=(hext29.replace(rip_0b(31),str(hex(rip_0b(31))))
+hext31=(hext30.replace(rip_0b(32),str(hex(rip_0b(32))))
+hext32=(hext31.replace(rip_0b(33),str(hex(rip_0b(33))))
+hext33=(hext32.replace(rip_0b(34),str(hex(rip_0b(34))))
+hext34=(hext33.replace(rip_0b(35),str(hex(rip_0b(35))))
+hext35=(hext34.replace(rip_0b(36),str(hex(rip_0b(36))))
+hext36=(hext35.replace(rip_0b(37),str(hex(rip_0b(37))))
+hext37=(hext36.replace(rip_0b(38),str(hex(rip_0b(38))))
+hext38=(hext37.replace(rip_0b(39),str(hex(rip_0b(39))))
+hext39=(hext38.replace(rip_0b(40),str(hex(rip_0b(40))))
+hext40=(hext39.replace(rip_0b(41),str(hex(rip_0b(41))))
+hext41=(hext40.replace(rip_0b(42),str(hex(rip_0b(42))))
+hext42=(hext41.replace(rip_0b(43),str(hex(rip_0b(43))))
+hext43=(hext42.replace(rip_0b(44),str(hex(rip_0b(44))))
+hext44=(hext43.replace(rip_0b(45),str(hex(rip_0b(45))))
+hext45=(hext44.replace(rip_0b(46),str(hex(rip_0b(46))))
+hext46=(hext45.replace(rip_0b(47),str(hex(rip_0b(47))))
+hext47=(hext46.replace(rip_0b(48),str(hex(rip_0b(48))))
+hext48=(hext47.replace(rip_0b(49),str(hex(rip_0b(49))))
+hext49=(hext48.replace(rip_0b(50),str(hex(rip_0b(50))))
+hext50=(hext49.replace(rip_0b(51),str(hex(rip_0b(51))))
+hext51=(hext50.replace(rip_0b(52),str(hex(rip_0b(52))))
+hext52=(hext51.replace(rip_0b(53),str(hex(rip_0b(53))))
+hext53=(hext52.replace(rip_0b(54),str(hex(rip_0b(54))))
+hext54=(hext53.replace(rip_0b(55),str(hex(rip_0b(55))))
+hext55=(hext54.replace(rip_0b(56),str(hex(rip_0b(56))))
+hext56=(hext55.replace(rip_0b(57),str(hex(rip_0b(57))))
+hext57=(hext56.replace(rip_0b(58),str(hex(rip_0b(58))))
+hext58=(hext57.replace(rip_0b(59),str(hex(rip_0b(59))))
+hext59=(hext58.replace(rip_0b(60),str(hex(rip_0b(60))))
+hext60=(hext59.replace(rip_0b(61),str(hex(rip_0b(61))))
+hext61=(hext60.replace(rip_0b(62),str(hex(rip_0b(62))))
+hext62=(hext61.replace(rip_0b(63),str(hex(rip_0b(63))))
+hext63=(hext62.replace(rip_0b(64),str(hex(rip_0b(64))))
+hext64=(hext63.replace(rip_0b(65),str(hex(rip_0b(65))))
+hext65=(hext64.replace(rip_0b(66),str(hex(rip_0b(66))))
+hext66=(hext65.replace(rip_0b(67),str(hex(rip_0b(67))))
+hext67=(hext66.replace(rip_0b(68),str(hex(rip_0b(68))))
+hext68=(hext67.replace(rip_0b(69),str(hex(rip_0b(69))))
+hext69=(hext68.replace(rip_0b(70),str(hex(rip_0b(70))))
+hext70=(hext69.replace(rip_0b(71),str(hex(rip_0b(71))))
+hext71=(hext70.replace(rip_0b(72),str(hex(rip_0b(72))))
+hext72=(hext71.replace(rip_0b(73),str(hex(rip_0b(73))))
+hext73=(hext72.replace(rip_0b(74),str(hex(rip_0b(74))))
+hext74=(hext73.replace(rip_0b(75),str(hex(rip_0b(75))))
+hext75=(hext74.replace(rip_0b(76),str(hex(rip_0b(76))))
+hext76=(hext75.replace(rip_0b(77),str(hex(rip_0b(77))))
+hext77=(hext76.replace(rip_0b(78),str(hex(rip_0b(78))))
+hext78=(hext77.replace(rip_0b(79),str(hex(rip_0b(79))))
+hext79=(hext78.replace(rip_0b(80),str(hex(rip_0b(80))))
+hext80=(hext79.replace(rip_0b(81),str(hex(rip_0b(81))))
+hext81=(hext80.replace(rip_0b(82),str(hex(rip_0b(82))))
+hext82=(hext81.replace(rip_0b(83),str(hex(rip_0b(83))))
+hext83=(hext82.replace(rip_0b(84),str(hex(rip_0b(84))))
+hext84=(hext83.replace(rip_0b(85),str(hex(rip_0b(85))))
+hext85=(hext84.replace(rip_0b(86),str(hex(rip_0b(86))))
+hext86=(hext85.replace(rip_0b(87),str(hex(rip_0b(87))))
+hext87=(hext86.replace(rip_0b(88),str(hex(rip_0b(88))))
+hext88=(hext87.replace(rip_0b(89),str(hex(rip_0b(89))))
+hext89=(hext88.replace(rip_0b(90),str(hex(rip_0b(90))))
+hext90=(hext89.replace(rip_0b(91),str(hex(rip_0b(91))))
+hext91=(hext90.replace(rip_0b(92),str(hex(rip_0b(92))))
+hext92=(hext91.replace(rip_0b(93),str(hex(rip_0b(93))))
+hext93=(hext92.replace(rip_0b(94),str(hex(rip_0b(94))))
+hext94=(hext93.replace(rip_0b(95),str(hex(rip_0b(95))))
+hext95=(hext94.replace(rip_0b(96),str(hex(rip_0b(96))))
+hext96=(hext95.replace(rip_0b(97),str(hex(rip_0b(97))))
+hext97=(hext96.replace(rip_0b(98),str(hex(rip_0b(98))))
+hext98=(hext97.replace(rip_0b(99),str(hex(rip_0b(99))))
+hext99=(hext98.replace(rip_0b(100),str(hex(rip_0b(100))))
+print(hext99)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
